4c31529a709bcaaa6ca2a4d4686e3915bcd26e48
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-02-27 11:02:56 +0700

    Added basic HTTP response logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{
+	io::prelude::*,
+	net::{TcpListener},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:12345").unwrap();
+
+	let mut connections: usize = 0;
+	for stream in listener.incoming() {
+		connections += 1;
+		println!("Connection established! #{connections}");
+		let mut stream = stream.unwrap();
+
+		let response = "HTTP/1.1 200 OK\r\n\
+			Content-Length: 10\r\n\r\n\
+			Halo Dunia";
+		stream.write_all(response.as_bytes()).unwrap();
+	}
 }
