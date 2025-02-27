6bb3df1ac9a6697f5dba925750cb37b86ec6aaac
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-02-27 18:05:14 +0700

    Add basic HTML page

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-	io::prelude::*,
+	fs,
+	io::prelude::Write,
 	net::{TcpListener},
 };
 
@@ -7,14 +8,21 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:12345").unwrap();
 
 	let mut connections: usize = 0;
+
 	for stream in listener.incoming() {
 		connections += 1;
 		println!("Connection established! #{connections}");
+
 		let mut stream = stream.unwrap();
 
-		let response = "HTTP/1.1 200 OK\r\n\
-			Content-Length: 10\r\n\r\n\
-			Halo Dunia";
+		let content: String = fs::read_to_string("index.html").unwrap();
+
+		let response = format!(
+			"HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+			content.len(),
+			content,
+		);
+
 		stream.write_all(response.as_bytes()).unwrap();
 	}
 }
